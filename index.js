d9b06d11c6f105fdb35a975361a7c7fe5eba2eaf
Ryan <ryanpaynt@uncc.edu>
2021-06-24 11:49:28 -0400

    staged for git

---
+++ index.js
@@ -0,0 +1,12 @@
+// Error: variable is defined but never used
+// eslint-disable-next-line no-unused-vars
+const example = true;
+
+// Error: function is empty
+// eslint-disable-next-line no-empty-function
+const sayHello = () => {
+
+};
+
+// Error: strings should use single quotes
+sayHello('hello');
